bd75d4ace6002be836dc958af1193e65efacc481
Hassan Jama <hassanjm97@gmail.com>
2026-03-26 09:30:23 +0000

    update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,13 +6,14 @@ export default function NavBar() {
         <header className="bg-white border-b shadow-sm mb-6">
       <div className="max-w-4xl mx-auto p-4 flex flex-col sm:flex-row items-center sm:justify-between gap-4">
 
-        <h1 className="text-2xl font-bold">Studdy Buddies</h1>
+        <h1 className="text-2xl font-bold">Study Buddies</h1>
 
         <nav className="flex flex-wrap justify-center sm:justify-end gap-4 text-blue-600">
           <Link href="/">Home</Link>
           <Link href="/groups">View All Groups</Link>
           <Link href="/groups/new">Create Group</Link>
-          <Link href="/">My Profile</Link>
+          <Link href="/users">All Users</Link>
+          <Link href="/users/you">My Profile</Link>
         </nav>
 
        <div className="flex gap-3 text-blue-600">
