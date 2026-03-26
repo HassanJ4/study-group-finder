4d5090e953d294a3707dc107de4579e047036fa7
callum withington <callumwithington2006@gmail.com>
2026-03-26 10:46:19 +0000

    color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div>StudyBuddies</div>
+import Image from "next/image";
+
+export default function home() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Image src="/logo.png" alt="My photo" width={500} height={500} />
+    </div>
+  );
 }
